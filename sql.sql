8372057c9f5615e8de59fd0e1f7a36433625378c
Denis Prado <denishenrique60@gmail.com>
2019-12-27 17:13:50 -0300

    SQL

---
+++ sql.sql
@@ -0,0 +1,15 @@
+/* erro caching sha2 password */
+
+alter user 'meuusuario'@'localhost' identified with mysql_native_password by 'minhasenha';
+
+CREATE DATABASE crud;
+
+CREATE TABLE produtos
+	(
+		codigo	INT NOT NULL AUTO_INCREMENT,
+		nome	VARCHAR(60),
+		preco	DECIMAL(4,2)
+
+	);
+	
+
