26b89676f217cf324c29b86df4727c19a0d8fb76
Denis Henrique Fernandes Prado <dhprado@dominio.fmu.br>
2019-12-27 15:19:50 -0300

    Add página inicial

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/stylesheet.css">
+	<title>Páginal inicial</title>
+</head>
+<body>
+	<header class="cabecalho">
+		<h1>CRUD - Create | Read | Update | Delete</h1>
+	</header>
+	<main>
+		<div class="item">
+			<div class="titulo-item">
+				<h2>Produtos</h2>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="view_listar.php">Visualizar produtos</a></li>
+					<li><a href="view_adicionar.php">Cadastrar produtos</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		<div class="item">
+			<div class="titulo-item">
+				<h2>Clientes</h2>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="view_listar.php">Visualizar clientes</a></li>
+					<li><a href="view_adicionar.php">Cadastrar produtos</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		<div class="item">
+			<div class="titulo-item">
+				<h2>Fornecedores</h2>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="view_listar.php">Visualizar fornecedores</a></li>
+					<li><a href="view_adicionar.php">Cadastrar fornecedores</a></li>
+				</ul>
+			</nav>
+		</div>
+	</main>
+</body>
+</html>
