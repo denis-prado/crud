e0ba6323a4d6c077e6e8985a30fc40a22ac8efd0
Denis Prado <denishenrique60@gmail.com>
2020-01-13 06:16:54 -0300

    Atualização de senha

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli('localhost', 'root', '1234', 'crud');
+$conn = new mysqli('localhost', 'root', 'De@denis2019', 'crud');
 
 if ($conn->connect_errno){
     die('Falha ao conectar no banco: (' .$conn->connect_errno. ')' .$conn->connect_error);
