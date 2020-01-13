050200400978187ce937329b16ee479a1f1b6823
Denis Prado <denishenrique60@gmail.com>
2020-01-13 06:35:39 -0300

    Validação do campo $preco

--- adicionar.php
+++ adicionar.php
@@ -7,37 +7,53 @@ if ($_POST)
 	$nome = $_POST['nome'];
 	$preco = $_POST['preco'];
 
-	if ()
+	if ($preco > 0){
 
-	$menssagem = "";
+		$menssagem = "";
 
-	$sql = 'INSERT INTO produtos (nome, preco) VALUES ( ?, ?)';
+		$sql = 'INSERT INTO produtos (nome, preco) VALUES ( ?, ?)';
 
-	$stmt = $conn->prepare($sql);
-	$stmt->bind_param('sd', $nome, $preco);
-	$stmt->execute();
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param('sd', $nome, $preco);
+		$stmt->execute();
 
-	$menssagem .= "<div class='sucesso'>";
-	$menssagem .= "<h2>Dados inseridos com sucesso!</h2>";
+		$menssagem .= "<div class='sucesso'>";
+		$menssagem .= "<h2>Dados inseridos com sucesso!</h2>";
 
-	$menssagem .= "<div class='menssagem'>";
-	$menssagem .= "<ul>";
-	$menssagem .= "<li>Nome: <strong>".$nome."</strong></li>";
-	$menssagem .= "<li>Preço <strong>R$ ".number_format($preco, 2, ',', '.')."</strong></li>";
-	$menssagem .= "</ul>";
-	$menssagem .= "</div>";
+		$menssagem .= "<div class='menssagem-sucesso'>";
+		$menssagem .= "<ul>";
+		$menssagem .= "<li>Nome: <strong>".$nome."</strong></li>";
+		$menssagem .= "<li>Preço <strong>R$ ".number_format($preco, 2, ',', '.')."</strong></li>";
+		$menssagem .= "</ul>";
+		$menssagem .= "</div>";
 
-	$menssagem .= "<div class='rodape'>";
-	$menssagem .= "<a class='btn' href='view_adicionar.php'>Ok</a>";
-	$menssagem .= "</div>";
-	$menssagem .= "</div>";
+		$menssagem .= "<div class='rodape'>";
+		$menssagem .= "<a class='btn' href='view_adicionar.php'>Ok</a>";
+		$menssagem .= "</div>";
+		$menssagem .= "</div>";
 
-	$stmt->close();
+		$stmt->close();
+	}
+	else {
+		$menssagem .= "<div class='erro'>";
+		$menssagem .= "<h2>Erro ao inserir os dados!</h2>";
+		$menssagem .= "<div class='menssagem-erro'>";
+		$menssagem .= "<p>Erro: <strong>".$preco."</strong> (Preço R$) incorreto</p>";
+		$menssagem .= "</div>";
+
+		$menssagem .= "<div class='rodape'>";
+		$menssagem .= "<a class='btn' href='view_adicionar.php'>Ok</a>";
+		$menssagem .= "</div>";
+		$menssagem .= "</div>";
+	}
 }
 else
 {
 	$menssagem .= "<div class='erro'>";
 	$menssagem .= "<h2>Erro ao inserir os dados!</h2>";
+	$menssagem .= "<div class='menssagem-erro'>";
+	$menssagem .= "<strong>Formulário preenchido de forma incorreta</strong>";
+	$menssagem .= "</div>";
 	$menssagem .= "</div>";
 }
 ?>
